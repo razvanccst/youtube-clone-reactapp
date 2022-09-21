ffc3075cf6f8beffcfcb1a061a8bd12dc344af04
razvanccst <48159550+razvanccst@users.noreply.github.com>
2022-09-21 14:45:01 +0300

    Feed Component Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import {Box} from '@mui/material'
-import Navbar from './components/Navbar'
-import Feed from './components/Feed'
-import VideoDetail from './components/VideoDetail'
-import ChannelDetail from './components/ChannelDetail'
-import SearchFeed from './components/SearchFeed'
+import {Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed} from './components'
+
 
 
 function App() {
